2625db657e48badd5249b4373eb1b665e3c2d164
Anna <anna.svrva@gmail.com>
2022-07-11 16:48:09 +0300

    updated

--- src/script.js
+++ src/script.js
@@ -75,7 +75,7 @@ function displayData(response) {
   dateElement.innerHTML = formatDay(response.data.dt * 1000);
   iconElement.setAttribute(
     "src",
-    `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+    `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
   );
   iconElement.setAttribute("alt", response.data.weather[0].description);
 }
